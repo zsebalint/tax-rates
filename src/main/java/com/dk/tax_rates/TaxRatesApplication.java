744767f02a80cb70990daa359a51f3a1def847c0
Balint Zsilinszki <balint.zsilinszki@cellpointmobile.com>
2024-07-24 12:43:53 +0200

    initial spring commit

---
+++ src/main/java/com/dk/tax_rates/TaxRatesApplication.java
@@ -0,0 +1,13 @@
+package com.dk.tax_rates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxRatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxRatesApplication.class, args);
+	}
+
+}
