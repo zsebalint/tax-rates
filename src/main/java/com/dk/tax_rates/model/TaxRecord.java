6d0cd4f69436d52f45bb1528d84d7ee3d4732335
Balint Zsilinszki <balint.zsilinszki@cellpointmobile.com>
2024-07-27 16:04:50 +0200

    implementation

--- src/main/java/com/dk/tax_rates/model/TaxRecord.java
+++ src/main/java/com/dk/tax_rates/model/TaxRecord.java
@@ -1,10 +1,16 @@
 package com.dk.tax_rates.model;
 
 import jakarta.persistence.*;
-import java.time.LocalDate;
-import lombok.Data;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
 
-@Data
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @Entity
 @Table(
     uniqueConstraints = @UniqueConstraint(columnNames = {"municipality", "validFrom", "validTo"}))
@@ -17,4 +23,20 @@ public class TaxRecord {
   private LocalDate validFrom;
   private LocalDate validTo;
   private Double rate;
+
+  @Override
+  public final boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null) return false;
+    Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+    Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+    if (thisEffectiveClass != oEffectiveClass) return false;
+    TaxRecord taxRecord = (TaxRecord) o;
+    return getId() != null && Objects.equals(getId(), taxRecord.getId());
+  }
+
+  @Override
+  public final int hashCode() {
+    return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+  }
 }
